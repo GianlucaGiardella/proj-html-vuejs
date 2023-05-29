2fd0fdb1f40fe95ee4495321d8216f648d67116f
GianluGiarde <gianlugiarde@gmail.com>
2023-05-29 14:50:45 +0200

    add: fontawesome solid

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import App from "./App.vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { far } from "@fortawesome/free-regular-svg-icons";
-import { brands } from "@fortawesome/free-brands-svg-icons";
-library.add(far, brands);
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(far, fas, fab);
 
 createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
