b289279402c8faf006d4f6a7a85a39dc6edce3aa
GianluGiarde <gianlugiarde@gmail.com>
2023-05-29 14:57:23 +0200

    add: info, logo & menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,34 @@
 <script>
-export default {};
+export default {
+  props: {
+    nav: Array,
+  },
+};
 </script>
 
-<template></template>
+<template>
+  <header>
+    <div class="info">
+      <p><span>(001) 88451234</span> <span>88455438</span></p>
+      <font-awesome-icon :icon="['fab', 'facebook-f']" />
+      <font-awesome-icon :icon="['fab', 'twitter']" />
+      <font-awesome-icon :icon="['fab', 'pinterest-p']" />
+      <font-awesome-icon :icon="['far', 'magnifying-glass']" />
+    </div>
+    <nav>
+      <div class="logo">
+        <img src="/logo.png" alt="Phlox logo" />
+      </div>
+      <ul class="menu">
+        <li v-for="link in nav">{{ link }}</li>
+      </ul>
+      <div class="purchase btn">Purchase</div>
+      <div class="search">
+        <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+      </div>
+    </nav>
+    <div class="jumbo"></div>
+  </header>
+</template>
 
 <style lang="scss" scoped></style>
