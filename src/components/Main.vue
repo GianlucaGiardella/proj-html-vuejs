b38b0d60a59811cfd5c8422a627f32c805800df4
GianluGiarde <gianlugiarde@gmail.com>
2023-05-30 12:53:47 +0200

    add: main components

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,32 @@
 <script>
-export default {};
+import Design from "./main-components/Design.vue";
+import About from "./main-components/About.vue";
+import Projects from "./main-components/Projects.vue";
+import Feedback from "./main-components/Feedback.vue";
+import News from "./main-components/News.vue";
+import Start from "./main-components/Start.vue";
+
+export default {
+  components: {
+    Design,
+    About,
+    Projects,
+    Feedback,
+    News,
+    Start,
+  },
+};
 </script>
 
-<template></template>
+<template>
+  <main>
+    <Design />
+    <About />
+    <Projects />
+    <Feedback />
+    <News />
+    <Start />
+  </main>
+</template>
 
 <style lang="scss" scoped></style>
