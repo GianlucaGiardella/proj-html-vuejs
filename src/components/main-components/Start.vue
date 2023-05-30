e6891a159565794ed841ca091d58eee6cec93f82
GianluGiarde <gianlugiarde@gmail.com>
2023-05-31 01:55:36 +0200

    add: start template & style

--- src/components/main-components/Start.vue
+++ src/components/main-components/Start.vue
@@ -2,6 +2,115 @@
 export default {};
 </script>
 
-<template></template>
+<template>
+  <section class="container">
+    <div class="ready">
+      <div class="card new-project black-gradient">
+        <p>Are You Ready?</p>
+        <h3>Start a New Project</h3>
+        <form>
+          <input type="email" placeholder="Enter Your Email Address" />
+          <button type="button">Submit</button>
+        </form>
+      </div>
+      <div class="card start-now green-gradient">
+        <p>What Are You Waiting For?</p>
+        <h3>Let's Talk About Work</h3>
+        <button class="start btn black-gradient">Start Now</button>
+      </div>
+    </div>
+    <div class="sponsor">
+      <img src="../../assets/images/sponsor-01.png" alt="sponsor-01" />
+      <img src="../../assets/images/sponsor-02.png" alt="sponsor-02" />
+      <img src="../../assets/images/sponsor-03.png" alt="sponsor-03" />
+      <img src="../../assets/images/sponsor-04.png" alt="sponsor-04" />
+      <img src="../../assets/images/sponsor-05.png" alt="sponsor-05" />
+      <img src="../../assets/images/sponsor-06.png" alt="sponsor-06" />
+    </div>
+  </section>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  padding: 6rem 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 6rem;
+}
+
+.container {
+  .ready {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    gap: 2rem;
+  }
+
+  .ready {
+    .card {
+      height: 330px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 1.5rem;
+      border-radius: 1rem;
+    }
+
+    .card {
+      p {
+        font-size: 0.8rem;
+        color: #ffffff80;
+      }
+
+      h3 {
+        color: #ffffff;
+        font-size: 2.3rem;
+        margin-bottom: 2.5rem;
+      }
+    }
+
+    .new-project {
+      form {
+        width: 450px;
+        display: flex;
+        border-radius: 5rem;
+        background-color: #70707059;
+        overflow: hidden;
+      }
+      input {
+        flex: 1 0 0;
+        padding: 1rem;
+        font-size: 0.8rem;
+        outline: none;
+        border-style: unset;
+        color: #aaaaaa;
+        background-color: #70707059;
+      }
+      button {
+        padding: 1rem;
+        font-size: 0.7rem;
+        text-transform: uppercase;
+        border-style: unset;
+        color: #aaaaaa;
+        background-color: #70707059;
+      }
+
+      button:hover {
+        cursor: pointer;
+        color: #ffffff;
+      }
+    }
+  }
+
+  .sponsor {
+    display: grid;
+    grid-template-columns: repeat(6, minmax(0, 1fr));
+    place-items: center;
+    gap: 4rem;
+  }
+
+  .sponsor > img:hover {
+    cursor: pointer;
+  }
+}
+</style>
