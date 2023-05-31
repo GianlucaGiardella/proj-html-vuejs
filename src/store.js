7e7200a8a1f64c8309a54b196fe5a3220b1d5015
GianluGiarde <gianlugiarde@gmail.com>
2023-05-31 14:28:53 +0200

    add: store

---
+++ src/store.js
@@ -0,0 +1,54 @@
+import { reactive } from "vue";
+
+export default reactive({
+  arrShowcase: [
+    {
+      id: 1,
+      name: "Basket Of Flowers",
+      type: "Marketing",
+      src: "showcase-01.jpg",
+    },
+    {
+      id: 2,
+      name: "A Famous Wheel",
+      type: "Marketing",
+      src: "showcase-02.jpg",
+    },
+    {
+      id: 3,
+      name: "Yellow Shoes",
+      type: "Marketing",
+      src: "showcase-03.jpg",
+    },
+    {
+      id: 4,
+      name: "Work Desk",
+      type: "Marketing",
+      src: "showcase-04.jpg",
+    },
+    {
+      id: 5,
+      name: "Paper Boats",
+      type: "Marketing",
+      src: "showcase-05.jpg",
+    },
+    {
+      id: 6,
+      name: "Sofa",
+      type: "Marketing",
+      src: "showcase-06.jpg",
+    },
+    {
+      id: 7,
+      name: "Hot Air Balloons",
+      type: "Marketing",
+      src: "showcase-07.jpg",
+    },
+    {
+      id: 8,
+      name: "Blue Orange",
+      type: "Marketing",
+      src: "showcase-08.jpg",
+    },
+  ],
+});
